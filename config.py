658827de374895fb1a834568b8af7a7ad46d7690
liulong <1161515052@qq.com>
2018-05-20 22:46:29 +0800

    flask

---
+++ config.py
@@ -0,0 +1,16 @@
+# encoding: utf-8
+import os
+
+DEBUG = True
+SECRET_KEY = os.urandom(24)
+
+HOST = '127.0.0.1'
+PORT = '3306'
+DATABASE = 'test'
+USERNAME = 'root'
+PASSWORD = 'liulong'
+DB_URI = 'mysql+mysqldb://{}:{}@{}:{}/{}?charset=utf8'.format(USERNAME, PASSWORD,
+                                                              HOST, PORT, DATABASE)
+
+SQLALCHEMY_DATABASE_URI = DB_URI
+SQLALCHEMY_TRACK_MODIFICATIONS = False
